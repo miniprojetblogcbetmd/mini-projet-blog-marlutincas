416b9321bd469ce507a054c137bbb19fdd666d00
CORALIE <coralie.baffou@laposte.net>
2016-05-26 15:31:07 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$DB_serveur = 'localhost'; // Nom du serveur
+$DB_utilisateur = 'root'; // Nom de l'utilisateur de la base
+$DB_motdepasse = ''; // Mot de passe pour accèder à la base
+$DB_base = 'projetnfa_83'; // Nom de la base
+
+$connection = mysql_connect($DB_serveur, $DB_utilisateur, $DB_motdepasse) // On se connecte au serveur
+or die (mysql_error().' sur la ligne '.__LINE__);
+
+mysql_select_db($DB_base, $connection) // On se connecte à la BDD
+or die (mysql_error().' sur la ligne '.__LINE__);
+?>
